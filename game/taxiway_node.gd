8ff4fd5d4d478dcd1659a4bb05eac726a4b7b5c6
Frejbo <frej.hellsten@gmail.com>
2024-01-09 14:32:49 +0100

    visualiserar nu pathen i taxiway debug

--- game/taxiway_node.gd
+++ game/taxiway_node.gd
@@ -5,21 +5,17 @@ class_name taxiway
 
 @export var taxiway_name: String :
 	set(text):
-		taxiway_name = text.to_upper()
+		name = text.to_upper()
+		taxiway_name = name
 @export var allow_y : bool = false
 
 @export_category("editor")
-@export var polygon_shape : PackedVector2Array:
-	set(shape):
-		csg_debug_shape.polygon = shape
-	get:
-		return polygon_shape
+@export var polygon_shape : PackedVector2Array
 
 var csg_debug_shape : CSGPolygon3D
 
 func _on_curve_changed():
-	print("HEJ")
-	#if !curve: return
+	if !curve: return
 	if allow_y: return
 	
 	# Force y of every point to 0
@@ -29,12 +25,14 @@ func _on_curve_changed():
 			curve.set_point_position(i, Vector3(point.x, 0, point.z))
 
 func _enter_tree():
-	csg_debug_shape = CSGPolygon3D.new()
-	#csg_debug_shape.set_polygon(polygon_shape)
-	csg_debug_shape.mode = CSGPolygon3D.MODE_PATH
-	csg_debug_shape.path_node = self.get_path()
-	add_child(csg_debug_shape)
+	for child in get_children():
+		if child is CSGPolygon3D:
+			child.free()
 	
-	if !Engine.is_editor_hint():
-		print("Removing mesh")
-		$CSGPolygon3D.free()
+	if Engine.is_editor_hint():
+		csg_debug_shape = CSGPolygon3D.new()
+		csg_debug_shape.polygon = polygon_shape
+		csg_debug_shape.mode = CSGPolygon3D.MODE_PATH
+		add_child(csg_debug_shape)
+		csg_debug_shape.path_node = self.get_path()
+		csg_debug_shape.owner = self
