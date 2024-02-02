c72c09876e06c0492cf4e844b07f7da680241679
Frejbo <frej.hellsten@gmail.com>
2024-02-03 00:43:29 +0100

    The lift equation works better now...

--- game/FlightPhysics.gd
+++ game/FlightPhysics.gd
@@ -6,12 +6,13 @@ class_name FlightPhysics
 @export var max_thrust_N : int = 200000
 ## Abstract value to determine how fast the engines should react to thrust lever changes.[br]This won't translate to any real values.
 @export var engine_spool_up_speed : float = .5
-## The position of thrust the lever. 0 = 0% thrust, 1 = 100% thrust.[br]The actual thrust output is determined by a number of factors and can be accessed via get_forces().
+## The position of thrust the lever. 0 = 0% thrust, 1 = 100% thrust.[br]The actual thrust output is determined by a number of factors and can be accessed via get_forces().[br][br]Note: You probably want to adjust this in-game.
 @export_range(0, 1) var thrust_lever := 0.0
 @export_category("Lift")
-@export var Coefficient_of_lift : float = 1.75
 ## The total area of the wing given in square meters.
 @export var wing_area : int = 100
+## The coefficient of lift (y axis) usually ranges from -1 - 2.[br]The Alpha/Angle of Attack degrees (x axis) ranging from -10° to 20° (0.0=-10°, 1.0=20°
+@export var Cl_v_Alpha := Curve.new()
 @export_category("Taxi speeds")
 ## The speed in knots which the aircraft will aim to be at in turns.
 @export var turn_speed_kts : int = 8
@@ -28,17 +29,24 @@ func ms_to_kts(meter_per_second : float) -> float:
 
 
 func _physics_process(delta: float) -> void:
+	manual_pitch_input()
 	current_thrust_force = lerp(current_thrust_force, max_thrust_N * thrust_lever, engine_spool_up_speed * delta)
 
 var total_time : float = Time.get_ticks_msec() * 0.001
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	print("Speed: ", round(ms_to_kts(linear_velocity.length())), " kts.")
+	%Speed.text = "Speed: " + str(round(ms_to_kts(linear_velocity.length()))) + " kts"
 	var delta : float = (Time.get_ticks_msec() * 0.001) - total_time
 	total_time = Time.get_ticks_msec() * 0.001
 	
-	var lift = (get_lift_force() / mass) * delta
-	print(lift)
-	state.linear_velocity += Vector3(0, lift, get_acceleration(delta)).rotated(Vector3(0, 1, 0), global_rotation.y)
+	var gravity_force : float = ProjectSettings.get_setting("physics/3d/default_gravity") * delta
+	state.linear_velocity.y -= gravity_force
+	
+	var lift_accel = (get_lift_force() / mass) * delta
+	state.linear_velocity.y += lift_accel
+	%TotalLift.text = "Total lift force: " + str(get_lift_force()) + " N"
+	%LiftAccel.text = "Lift acceleration: " + str(lift_accel) + " m/s"
+	%TotalLiftAccel.text = "Total lift acceleration: " + str(lift_accel - gravity_force) + " m/s"
+	state.linear_velocity += Vector3(0, 0, get_acceleration(delta)).rotated(Vector3(0, 1, 0), global_rotation.y)
 
 
 func get_acceleration(delta : float) -> float:
@@ -46,11 +54,22 @@ func get_acceleration(delta : float) -> float:
 	return (current_thrust_force / mass) * delta
 
 func get_lift_force() -> float:
-	var air_density := 0.0752
-	var velocity_squared = abs(linear_velocity.length() * linear_velocity.length())
-	#print(get_velocity())
-	var lift : float = Coefficient_of_lift * air_density * (velocity_squared / 2.0) * wing_area
-	var gravity_force : float = mass * ProjectSettings.get_setting("physics/3d/default_gravity")
+	# Calculate angle of attack
+	var AOA : float = rad_to_deg(-global_rotation.rotated(Vector3(0, 1, 0), global_rotation.y).z)
+	AOA = clamp(AOA, -10, 20) # Cl v A chart only includes these angles
+	var Cl := Cl_v_Alpha.sample((AOA + 10) / 30)
+	
+	var air_density := 0.5 # not simulating this nuh uh
+	var velocity_squared = abs(Vector2(linear_velocity.x, linear_velocity.z).length() * Vector2(linear_velocity.x, linear_velocity.z).length())
+	
+	var lift : float = Cl * (air_density / 2) * velocity_squared * wing_area
+	%LiftForce.text = "Lift force: " + str(lift) + " N"
+	
 	#print("lift: ", lift, " gravity force: ", gravity_force, " sum: ", lift - gravity_force)
-	return lift - gravity_force
+	return lift
 
+func manual_pitch_input() -> void:
+	if Input.is_action_pressed("ui_up"):
+		rotation_degrees += Vector3(0, 0, -.5).rotated(Vector3(0, 1, 0), global_rotation.y)
+	if Input.is_action_pressed("ui_accept"):
+		rotation_degrees += Vector3(0, 0, .5).rotated(Vector3(0, 1, 0), global_rotation.y)
