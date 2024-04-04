9756a3e520651a88315f393e450769d7286d732e
Frejbo <frej.hellsten@gmail.com>
2024-04-04 23:57:49 +0200

    Added pushback points and visualisation

--- game/Gate.gd
+++ game/Gate.gd
@@ -13,7 +13,25 @@ class_name Gate
 		stand_name = text.to_upper()
 
 @export var taxiway_in : taxiway = get_parent()
-@export var push_procedure : pushback_points
+@export var push_procedure : pushback_points:
+	set(val):
+		push_procedure = val
+		if val != null:
+			val.updated_points.connect(preview_pushback_points)
+
+@export var pushpoint_preview : PackedScene
+func preview_pushback_points(points : Array) -> void:
+	for child in get_children():
+		if child.is_in_group("pushpoint preview"):
+			child.free()
+	
+	for p in points:
+		var scene = pushpoint_preview.instantiate()
+		scene.position.x = p.position.x
+		scene.position.z = p.position.y
+		scene.rotation_degrees.y = p.rotation
+		scene.owner = self
+		add_child(scene)
 
 func _enter_tree() -> void:
 	Game.stands[stand_name] = self
--- game/Pushback/pushback_points.gd
+++ game/Pushback/pushback_points.gd
@@ -1,8 +1,16 @@
+@tool
 extends Resource
 
 class_name pushback_points
+signal updated_points(Array)
 
-@export var points : Array[Transform3D]:
+@export var points : Array
+@export_group("Add new point")
+@export var new_rotation : float
+@export var new_position : Vector2
+@export var add_point_using_rotation_and_position : bool:
 	set(val):
-		print(val)
-		points = val
+		points.append({"position":new_position, "rotation":new_rotation})
+		updated_points.emit(points)
+		add_point_using_rotation_and_position = false
+		print("Added pushback point with position ", new_position, " and rotation ", new_rotation)
